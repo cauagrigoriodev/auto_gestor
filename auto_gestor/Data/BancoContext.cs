b62c538d1aab8a32dd7dd12afbe96c134c5f7bd7
Cauã Grigorio <cauagrigorio55@gmail.com>
2025-03-20 09:52:45 -0300

    Atualização da pagina de cadastro dos alunos 2.0

---
+++ auto_gestor/Data/BancoContext.cs
@@ -0,0 +1,15 @@
+﻿using auto_gestor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace auto_gestor.Data
+{
+    public class BancoContext : DbContext
+    {
+        // configuração do construtor com o DbOptions como parâmetro de entrada.
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ClienteModel> ClienteModels { get; set; }
+    }
+}
